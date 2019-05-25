1d164d9609d4e795c2530e6fb2206f56999db8c9
Kerry Jiang <kerry-jiang@hotmail.com>
2019-05-24 17:25:26 -0700

    tried to implement ICSharpCoreEngine

--- src/ICSharpCore/ICSharpCoreEngine.cs
+++ src/ICSharpCore/ICSharpCoreEngine.cs
@@ -1,3 +1,5 @@
+using System;
+using ICSharpCore.Script;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Jupyter.Core;
@@ -6,15 +8,34 @@ namespace ICSharpCore
 {
     class ICSharpCoreEngine : BaseEngine
     {
+        private InteractiveScriptEngine _scriptEngine;
+        
         public ICSharpCoreEngine(IShellServer shell, IOptions<KernelContext> context, ILogger logger)
             : base(shell, context, logger)
         {
-
+            _scriptEngine = new InteractiveScriptEngine(AppContext.BaseDirectory, logger);
         }
 
         public override ExecutionResult ExecuteMundane(string input, IChannel channel)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var result = _scriptEngine.Execute(input);
+
+                return new ExecutionResult
+                    {
+                        Status = ExecuteStatus.Ok,
+                        Output = result
+                    };
+            }
+            catch (Exception e)
+            {
+                return new ExecutionResult
+                {
+                    Status = ExecuteStatus.Error,
+                    Output = e
+                };
+            }
         }
     }
 }
